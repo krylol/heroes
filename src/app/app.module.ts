f59df8042e2e63c196d474367cf6be58fe957e2e
krylol <pedroruizventura27@gmail.com>
2022-10-10 12:34:30 +0200

    as

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AltaComponent } from './heroes/componentes/alta/alta.component';
+import { BusquedaComponent } from './heroes/componentes/busqueda/busqueda.component';
+import { DetalleComponent } from './heroes/componentes/detalle/detalle.component';
+import { HomeComponent } from './heroes/componentes/home/home.component';
+import { ListadoComponent } from './heroes/componentes/listado/listado.component';
+import { ModalComponent } from './heroes/componentes/modal/modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AltaComponent,
+    BusquedaComponent,
+    DetalleComponent,
+    HomeComponent,
+    ListadoComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
